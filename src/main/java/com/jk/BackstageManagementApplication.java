5b9b2fcf5b46a3e73b81ba5168b4f99fdcfa9f32
安林晖 <alh2323860866@qq.com>
2018-08-09 16:30:08 +0800

    安林晖提交 启动类整合了支持JSP页面相关方法

--- src/main/java/com/jk/BackstageManagementApplication.java
+++ src/main/java/com/jk/BackstageManagementApplication.java
@@ -2,11 +2,25 @@ package com.jk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class BackstageManagementApplication {
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class BackstageManagementApplication extends SpringBootServletInitializer {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackstageManagementApplication.class, args);
 	}
+
+	//支持JSP页面
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(BackstageManagementApplication.class);
+	}
+
+
+
 }
